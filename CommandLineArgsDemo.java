458d9d7e15ae842d5f869d62845a159867b83193
SanaNasar <sanasehereen.cse@gmail.com>
2016-10-12 18:53:31 -0700

    adding command line args demo

---
+++ CommandLineArgsDemo.java
@@ -0,0 +1,13 @@
+/*
+  Usage: java CommandLineArgsDemo 200 10
+*/
+class CommandLineArgsDemo
+{
+    public static void main(String[] args)
+    {
+        //HelloWorld myHelloWorld = new HelloWorld();
+        int numerator = Integer.parseInt(args[0]);
+        int denominator = Integer.parseInt(args[1]);
+        System.out.println(numerator / denominator);
+    }
+}
