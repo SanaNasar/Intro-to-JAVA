3a6455a814c9491c5287ad02fa2972207a057828
SanaNasar <sanasehereen.cse@gmail.com>
2016-10-21 08:18:16 -0700

    Adding while loop demo

---
+++ WhileDemo.java
@@ -0,0 +1,15 @@
+// Demonstrate the while loop.
+class WhileDemo
+{
+  public static void main(String[] args)
+  {
+    char ch;
+    // print the alphabet using a while loop
+    ch = 'a'; // initialize the counter
+    while(ch <= 'z') // test the condition
+    {
+      System.out.print(ch);
+      ch++; // increment the counter
+    }
+  }
+}
